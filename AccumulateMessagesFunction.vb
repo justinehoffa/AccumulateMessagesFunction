22d9a6003296c8f86d17d3d1b79b146c21b4d448
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-13 20:36:07 -0600

    lol

--- AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunction.vb
@@ -1,7 +1,35 @@
 ﻿Module AccumulateMessagesFunction
 
     Sub Main()
+        Dim userInput As String
+        Dim message As String
+        Dim clearData As Boolean
+
+        Do
+            userInput = Console.ReadLine()
+            If userInput = "call" Then
+                MsgBox(message)
+            ElseIf userInput = "clear" Then
+                clearData = True
+            End If
+
+            message = AccumulateMessage(userInput, clearData)
+            clearData = False
+
+        Loop
 
     End Sub
+
+    Function AccumulateMessage(ByVal newMessage As String, ByVal clear As Boolean) As String
+        Static userMessage As String
+        If clear = True Then
+            userMessage = ""
+        ElseIf newMessage = "call" Then
+        Else
+            userMessage &= newMessage & vbNewLine
+        End If
+
+        Return userMessage
+    End Function
 
 End Module
