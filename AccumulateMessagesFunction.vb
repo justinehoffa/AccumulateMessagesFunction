9f0a0dd9e780b6b9b375689c6a45e2d3cd23f564
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-20 15:00:40 -0600

    l

--- AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunction.vb
@@ -1,4 +1,14 @@
-﻿Module AccumulateMessagesFunction
+﻿'Justine Hoffa
+'RCET0265
+'Fall2020
+'AccumulationMessagesFunction
+'https://github.com/justinehoffa/AccumulateMessagesFunction
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module AccumulateMessagesFunction
 
     Sub Main()
         Dim userInput As String
